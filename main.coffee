9db667fe2504939e4b32b6b1b0b4471537066319
koba789 <kobahide789@gmail.com>
2011-10-18 19:05:14 +0900

    頑張って敷き詰めた

--- main.coffee
+++ main.coffee
@@ -4,10 +4,14 @@ $ ()->
   ctx = canvas.getContext('2d')
   drawing = new Drawing(ctx)
 
-  hexagon = new Hexagon 100
-  trans = new Transform 50, 30, 45, 1.5
-  style = new Fill 'rgb(162, 19, 47)'
-  entity = new Entity hexagon, trans, style
-  drawing.add entity
+  for row in [-1..1]
+    for col in [-2..2]
+      y = row * 80 * 1.5
+      x = 40 * Math.sqrt(3) * (2 * col + row % 2)
+      hexagon = new Hexagon 80
+      trans = new Transform x, y, 30, 0.95
+      style = new Fill 'rgb(162, 19, 47)'
+      entity = new Entity hexagon, trans, style
+      drawing.add entity
 
   drawing.draw()
--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 (function() {
   $(function() {
-    var canvas, ctx, drawing, entity, hexagon, style, trans;
+    var canvas, col, ctx, drawing, entity, hexagon, row, style, trans, x, y;
     canvas = document.getElementById('canvas');
     if (!canvas || !canvas.getContext) {
       return false;
     }
     ctx = canvas.getContext('2d');
     drawing = new Drawing(ctx);
-    hexagon = new Hexagon(100);
-    trans = new Transform(50, 30, 45, 1.5);
-    style = new Fill('rgb(162, 19, 47)');
-    entity = new Entity(hexagon, trans, style);
-    drawing.add(entity);
+    for (row = -1; row <= 1; row++) {
+      for (col = -2; col <= 2; col++) {
+        y = row * 80 * 1.5;
+        x = 40 * Math.sqrt(3) * (2 * col + row % 2);
+        hexagon = new Hexagon(80);
+        trans = new Transform(x, y, 30, 0.95);
+        style = new Fill('rgb(162, 19, 47)');
+        entity = new Entity(hexagon, trans, style);
+        drawing.add(entity);
+      }
+    }
     return drawing.draw();
   });
 }).call(this);
